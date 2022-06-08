bfebc2af0f5ada3e465f8d8d048f3411e5657af4
Lucas Freitas Cordeiro <93345102+Bariand@users.noreply.github.com>
2022-06-08 08:09:00 -0300

    Add files via upload

---
+++ ASPNET_CRUD/Controllers/FormController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: FormController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: FormController/Create
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: FormController/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // POST: FormController/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
